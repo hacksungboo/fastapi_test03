2e725a651ee61afc395afa5c1d03544d28e9898d
hacksung <qngkrtjd@naver.com>
2026-04-21 09:29:40 +0900

    글 삭제기능 추가함

--- main.py
+++ main.py
@@ -62,22 +62,18 @@ def postNew(writer: str = Form(...), title: str = Form(...), content: str = Form
         (writer, title, content)
         VALUES(:writer, :title, :content)
     """)
+    # query 문을 실행하면서 같이 전달한 dict의 키값과 :writer, :title, :content 동일한 위치에 값이 바인딩되어서 실행된다.
     db.execute(query, {"writer":writer, "title":title, "content":content})
     db.commit()
 
     # 특정 경로로 요청을 다시 하도록 리다일렉트 응답을 준다. 
     return RedirectResponse("/post", status_code=302)
 
-# 게시글 삭제 요청 처리
-@app.post("/post/delete")
-def postDelete(num: int = Form(...), db: Session = Depends(get_db)):
-    # DB 에서 특정 번호의 글을 삭제하기 위한 sql 문 준비
-    query = text("""
-        DELETE FROM post
-        WHERE num = :num
-    """)
-    db.execute(query, {"num": num})
+@app.get("/post/delete/{num}") # {num} 경로 변수 선언 (path variable)
+def delete(num:int,db:Session=Depends(get_db)): # 경로 변수의 이름과 함수의 매개변수의 이름을 일치시킨다
+    # num 에는 삭제할 글의 번호가 들어 있다.
+    query=text("DELETE FROM post WHERE num=:num")
+    db.execute(query,{"num":num})
     db.commit()
-
-    # 삭제 후 목록 페이지로 리다이렉트
-    return RedirectResponse("/post", status_code=302)
+    # 클라이언트가 /post로 다시 요청하라고 강요하기
+    return RedirectResponse("/post",status_code=302)
